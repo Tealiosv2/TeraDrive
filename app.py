2856bd5d60d71db46dd8b14512d021315cc9bf87
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2023-10-16 15:56:55 -0700

    routing

--- app.py
+++ app.py
@@ -9,22 +9,13 @@ def get_db_connection():
         password="mysecretpassword",  # Use the password you set for the PostgreSQL container
         host="localhost",  # Docker container is running on your local machine
         port="5432",  # Default PostgreSQL port
-        database="mydatabase"  # Replace with your database name
+        database="teradrive"  # Replace with your database name
     )
     return connection
 
 @app.route('/')
 def index():
-    try:
-        connection = get_db_connection()
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM your_table")
-        data = cursor.fetchall()
-        cursor.close()
-        connection.close()
-        return str(data)
-    except (Exception, psycopg2.Error) as error:
-        return str(error)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
